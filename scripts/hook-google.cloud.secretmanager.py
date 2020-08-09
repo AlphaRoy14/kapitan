0b3126d30a1018fddbb73ca15a979092185ade88
Arindaam Roy <alpharoy14@gmail.com>
2020-08-09 21:34:02 +0530

    [testing] v32 pyinstaller

--- scripts/hook-google.cloud.secretmanager.py
+++ scripts/hook-google.cloud.secretmanager.py
@@ -1,5 +1,5 @@
 from PyInstaller.utils.hooks import copy_metadata, collect_data_files
 datas = copy_metadata('google-cloud-secret-manager')
-datas = copy_metadata('gcloud')
+datas += copy_metadata('gcloud')
 datas += copy_metadata('google-cloud-core')
 datas += collect_data_files('grpc')
